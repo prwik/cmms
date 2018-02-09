df5b832ccf91ef5115ebdab61ff7ab62de6b4f13
Javier Quintero <javierq@fb.com>
2018-02-08 19:28:21 -0800

    chage deploy route

--- cmmsApp.py
+++ cmmsApp.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import subprocess
 
 app = Flask(__name__)
 
@@ -9,7 +8,6 @@ def hello():
 
 @app.route("/deploy")
 def deploy():
-    subprocess.Popen('git pull')
     return "success"
 
 if __name__ == "__main__":
