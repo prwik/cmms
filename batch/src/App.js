ba2420f10916e57622305e9248acf86cecf0c4cd
Javier Quintero <javierq@fb.com>
2018-02-15 19:32:22 -0800

    adding conent to react app

--- batch/src/App.js
+++ batch/src/App.js
@@ -5,17 +5,45 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navbar />
+        <Content />
       </div>
     );
   }
 }
 
 export default App;
+
+class Navbar extends Component {
+  render() {
+    function click(){
+      console.log('click');
+    }
+    return (
+      <div className="navbar">
+        <div className="menu_button" onClick={click}>
+          <div className="menu_lines"></div>
+          <div className="menu_lines"></div>
+          <div className="menu_lines"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class SiteCard extends Component {
+  render(){
+    return (
+      <div className="card"></div>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return(
+      <SiteCard />
+    )
+  }
+}
