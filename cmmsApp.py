d4ddbf8752253c1b78fe23329bd45e18ef0893da
Javier Quintero <javierq@fb.com>
2018-02-25 10:14:57 -0800

    add sqlalchemy

--- cmmsApp.py
+++ cmmsApp.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from config import con_string
+
+engine = create_engine(con_string)
+
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    res = engine.execute('desc prod.sites').fetchall()
+    print res
     return "Flask Index Page"
 
 @app.route("/test")
