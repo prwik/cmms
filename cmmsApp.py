61522ee00cad3d7c6fb7e289ed55225d21165e85
Javier Quintero <javierq@fb.com>
2018-02-25 15:29:57 -0800

    add equipment route

--- cmmsApp.py
+++ cmmsApp.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from sqlalchemy import create_engine
 from config import con_string
+import json
 
 engine = create_engine(con_string)
 
@@ -10,13 +11,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    res = engine.execute('desc prod.sites').fetchall()
-    print res
     return "Flask Index Page"
 
-@app.route("/test")
-def deploy():
-    return "success"
+@app.route("/equipment/<site_id>")
+def deploy(site_id):
+    sql = "select name, description from prod.equipment where site_id={0}".format(site_id)
+    res = engine.execute(sql).fetchall()
+    response = []
+    for row in res:
+        response.append({'name': row[0], 'description': row[1]})
+    return json.dumps(response)
 
 if __name__ == "__main__":
     app.run()
